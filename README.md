3ceb7f41a0e4dfa9b451c8846f1345fcd7d13559
machock <229235844@qq.com>
2018-07-15 19:46:55 +0800

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# react 架构
+## 目录
++ 基础架构基于官方脚手架
++ 状态管理基于redux-saga
++ 路由 react-router
++ 请求基于原生fetch
++ 测试框架 mocha
++ 性能与上报
++ webpack打包设置
---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+class App extends Component {
+  render() {
+    return <div>App Page</div>;
+  }
+}
+App.propTypes = {};
+App.defaultProps = {};
+export default App;
